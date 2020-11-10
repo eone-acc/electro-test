3fee406b4783dc03197ae61d38c01a440b8d43f3
eone-acc <eone7096@gmail.com>
2020-11-10 19:53:25 +0400

    git commit -m "beta-release on firebase"

--- public/js/script.js
+++ public/js/script.js
@@ -500,4 +500,19 @@ submitBtn.addEventListener('click',async () => {
   }   
 })
 
-screen.lockOrientation('landscape');
+
+function toggleFullScreen() {
+  var doc = window.document;
+  var docEl = doc.documentElement;
+
+  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+  if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+    requestFullScreen.call(docEl);
+  }
+  else {
+    cancelFullScreen.call(doc);
+  }
+}
+toggleFullScreen()
